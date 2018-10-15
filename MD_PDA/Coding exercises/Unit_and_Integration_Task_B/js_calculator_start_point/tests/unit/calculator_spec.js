b9e48f191d7385c5c129b7d70ae1e7970e277be1
Mark Ditzel <mark.ditzel@ed.ac.uk>
2018-10-15 16:04:33 +0100

    double-checked that all PART B tests pass

--- MD_PDA/Coding exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ MD_PDA/Coding exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -10,34 +10,34 @@ describe('calculator', function () {
 
 // 1. calculator.add()
   it('1. can add numbers together', function () {
-    calculator.previousTotal = 10;
-    calculator.add(40);
+    calculator.previousTotal = 1;
+    calculator.add(4);
     result = calculator.runningTotal;
-    assert.equal(result, 50);
+    assert.equal(result, 5);
   })
 
 // 2. calculator.subtract()
   it('2. can subtract numbers', function () {
-    calculator.previousTotal = 6;
+    calculator.previousTotal = 7;
     calculator.subtract(4);
     result = calculator.runningTotal;
-    assert.equal(result, 2);
+    assert.equal(result, 3);
   })
 
   // 2. calculator.multiply()
   it('3. can multiply numbers', function () {
-    calculator.previousTotal = 4;
-    calculator.multiply(4);
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
     result = calculator.runningTotal;
-    assert.equal(result, 16);
+    assert.equal(result, 15);
   })
 
 // calculator.divide()
   it('4. can divide numbers', function () {
-    calculator.previousTotal = 30;
-    calculator.divide(6);
+    calculator.previousTotal = 21;
+    calculator.divide(7);
     result = calculator.runningTotal;
-    assert.equal(result, 5);
+    assert.equal(result, 3);
   })
 
 // calculator.numberClick multiples()
